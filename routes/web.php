af277e7a435eeb5f7231bf5c149a89e334a92b91
fal-line <mnaufal.ihza@gmail.com>
2025-07-24 23:39:29 +0700

    modify/ add auth for accesing dashbord

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,25 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+Auth::routes();
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', function () {
+    $user = Auth::user();
+
+    if ($user->ref_id_role === 1) {
+        return view('homeEmployee');
+    } elseif ($user->ref_id_role === 2) {
+        return view('homeClient');
+    }
+
+    abort(403); 
+    // Forbidden if role is not recognized
+})->middleware('auth');
