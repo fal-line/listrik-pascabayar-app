786160591c90dd4bef3ec3a253cca59f9a73b5ea
fal-line <mnaufal.ihza@gmail.com>
2025-07-31 00:54:27 +0700

    create/ client detail data

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -22,4 +22,20 @@ class ClientController extends Controller
 
         return view('employee.manageClient.clientsData', ['pelanggans' => $clients]);
     }
+
+    public function detail()
+    {
+        $clients = DB::table('users')
+        ->join('clients', 'clients.user_id', '=', 'users.id')
+        ->join('electricity_rates', 'clients.ref_id_electricityRate', '=', 'electricity_rates.id_tarif')
+        ->where('users.ref_id_role', 2)
+        ->select(
+            'users.*',
+            'clients.*',
+            'electricity_rates.*',
+        )
+        ->get();
+
+        return view('employee.manageClient.clientsData', ['pelanggans' => $clients]);
+    }
 }
