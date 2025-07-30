be6f11975f4137e518d6a147b6b37003b7bf8f0c
fal-line <mnaufal.ihza@gmail.com>
2025-07-31 00:54:44 +0700

    modify/ landing page adjustment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,18 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Selamat datang di Portal PLN</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                <div class="card-body row justify-content-center">
+                    
+                    Mohon untuk melakukan login terlebih dahulu untuk mengakses informasi layanan.
+
+                    @if (Route::has('login'))
+                    <br>
+                    <br>
+                            <a class="nav-link" href="{{ route('login') }}"> <h2 class="row justify-content-center">{{ __('Login') }}</h2></a>
+                    <br>
+                    <br>
                     @endif
 
                 </div>
